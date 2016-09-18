0258ab53910557c61a2a1869a17f61c0e6e19e52
Artyom Goncharov <m4yers@gmail.com>
2016-09-18 20:47:46 +0300

    [M680x0] Add Prologue/Epilogue
    NOT TESTED

--- lib/Target/M680x0/M680x0Subtarget.h
+++ lib/Target/M680x0/M680x0Subtarget.h
@@ -78,8 +78,13 @@ public:
   bool useSmallSection() const { return UseSmallSection; }
 
   bool abiUsesSoftFloat() const;
-  
-  unsigned stackAlignment() const { return 2; } // FIXME true for M68000
+
+  // FIXME true for M68000 and probably for all M680x0 line, but ColdFire
+  unsigned getStackAlignment() const { return 2; }
+
+  /// getSlotSize - Stack slot size in bytes.
+  /// TODO this must be controlled by options like -malign-int and -mshort
+  unsigned getSlotSize() const { return 2; }
 
   M680x0Subtarget &initializeSubtargetDependencies(StringRef CPU, StringRef FS,
                                                  const M680x0TargetMachine &TM);
