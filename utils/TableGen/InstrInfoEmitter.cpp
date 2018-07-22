4f43e563a875a53e21c7eeb0ebe270d8fab80b87
Artyom Goncharov <artyom.goncharov@guardsquare.com>
2018-07-22 20:17:08 +0200

    [M680x0] Post-merge test fixes.
    Control/setcc.ll still failing...

--- utils/TableGen/InstrInfoEmitter.cpp
+++ utils/TableGen/InstrInfoEmitter.cpp
@@ -400,7 +400,7 @@ void InstrInfoEmitter::emitOperandTypesEnum(raw_ostream &OS,
     if (!Op->isAnonymous())
       OS << "  " << Op->getName();
       if (First) {
-        OS << " = MCOI::OPERAND_FIRST_TARGET";
+        OS << " = llvm::MCOI::OPERAND_FIRST_TARGET";
         First = false;
       }
       OS << ",\n";
@@ -616,7 +616,7 @@ void InstrInfoEmitter::run(raw_ostream &OS) {
 
   emitOperandNameMappings(OS, Target, NumberedInstructions);
 
-  emitOperandTypesEnum(OS, Target);
+  // emitOperandTypesEnum(OS, Target);
 
   emitMCIIHelperMethods(OS);
 }
